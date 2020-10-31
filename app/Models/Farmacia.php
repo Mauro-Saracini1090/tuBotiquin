0d6e25a0669ecf3d77becc38c85f27f78da30dba
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-10-31 19:44:39 -0300

    Controller and Model for pharmacy

---
+++ app/Models/Farmacia.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Farmacia extends Model
+{
+    use HasFactory;
+    protected $table = "farmacia";
+    protected $primaryKey = 'id_farmacia';
+    
+    protected $fillable = [
+        'id_usuario',
+        'nombre_farmacia',
+        'cuit',
+        'habilidata',
+    ];
+
+    // Para obtener el dueno de la farmacia
+    public function usuarioFarmaceutico()
+    {
+        return $this->hasMany(Usuario::class,'id_usuario');
+    }
+
+    //Para obtener todas las sucursales
+    public function getSucursales()
+    {
+        return $this->belongsT(Sucursal::class,'id_farmacia');
+    }
+
+}
