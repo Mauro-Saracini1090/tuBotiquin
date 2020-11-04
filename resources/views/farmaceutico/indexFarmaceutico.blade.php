b4638241249c03f6feff08b78768bf2ee68707aa
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-11-04 19:31:48 -0300

    index farmaceutico (panel de opciones)

--- resources/views/farmaceutico/indexFarmaceutico.blade.php
+++ resources/views/farmaceutico/indexFarmaceutico.blade.php
@@ -8,31 +8,33 @@
                   <div class="row">
                     <div class="col-md-4  col-12">
                         <div class="shadow p-3 mb-5 bg-white rounded">
-                            <img class="card-img-top"  width="200" height="200" src="../public/assets/img/imgFarmaceutico/cargar.svg" alt="Card image cap">
+                            <img class="card-img-top"  width="150" height="150" src="../public/assets/img/imgFarmaceutico/cargar.svg" alt="Card image cap">
                                 <div class="card-body">
-                                    <h5 class="card-title">Cargar Farmacia</h5>
+                                    <h5 class="card-title">Farmacia</h5>
                                     <p class="card-text"></p> 
                                     <hr>
-                                    <a href="{{ route('farmacia.create') }}" class="btn btn-primary btn-sm">Ir</a>
+                                    <a href="{{ route('farmacia.create') }}" class="btn btn-primary btn-sm">Cargar</a>
+                                    <a href="" class="btn btn-primary btn-sm">Ver</a>
                                 </div>
                         </div>
                     </div>
                     <div class="col-md-4   col-12">
                         <div class="shadow p-3 mb-5 bg-white rounded">
-                            <img class="card-img-top" width="200" height="200" src="../public/assets/img/imgFarmaceutico/sucursales.svg" alt="Card image cap">
+                            <img class="card-img-top" width="150" height="150" src="../public/assets/img/imgFarmaceutico/sucursales.svg" alt="Card image cap">
                                 <div class="card-body">
-                                    <h5 class="card-title">Cargar Sucursal</h5>
+                                    <h5 class="card-title">Sucursal</h5>
                                     <p class="card-text"></p> 
                                     <hr>
-                                    <a href={{ route ('sucursal.create') }} class="btn btn-primary btn-sm">Ir</a>
+                                    <a href={{ route ('sucursal.create') }} class="btn btn-primary btn-sm">cargar</a>
+                                    <a href="" class="btn btn-primary btn-sm">Ver</a>
                                 </div>
                         </div>   
                     </div>
                     <div class="col-md-4   col-12">
                         <div class="shadow p-3 mb-5 bg-white rounded">
-                            <img class="card-img-top" width="200" height="200" src="../public/assets/img/imgFarmaceutico/reserva.svg"  alt="Card image cap" style="">
+                            <img class="card-img-top" width="150" height="150" src="../public/assets/img/imgFarmaceutico/reserva.svg"  alt="Card image cap" style="">
                                 <div class="card-body">
-                                    <h5 class="card-title">Responder Pedido</h5>
+                                    <h5 class="card-title">Pedidos</h5>
                                     <p class="card-text"></p>
                                     <hr>
                                     <a href="#" class="btn btn-primary btn-sm">Ir</a>
@@ -44,9 +46,9 @@
                   <div class="row">
                     <div class="col-md-4   col-12">
                         <div class="shadow p-3 mb-5 bg-white rounded">
-                            <img class="card-img-top" width="200" height="200" src="../public/assets/img/imgFarmaceutico/reserva.svg"  alt="Card image cap">
+                            <img class="card-img-top" width="150" height="150" src="../public/assets/img/imgFarmaceutico/reserva.svg"  alt="Card image cap">
                                 <div class="card-body">
-                                    <h5 class="card-title">Responder Reserva</h5>
+                                    <h5 class="card-title">Reservas</h5>
                                     <p class="card-text"></p> 
                                     <hr>
                                     <a href="#" class="btn btn-primary btn-sm">Ir</a>
@@ -55,18 +57,18 @@
                     </div>
                     <div class="col-md-4   col-12">
                         <div class="shadow p-3 mb-5 bg-white rounded">
-                            <img class="card-img-top" width="200" height="200" src="../public/assets/img/imgFarmaceutico/stock.svg" alt="Card image cap">
+                            <img class="card-img-top" width="150" height="150" src="../public/assets/img/imgFarmaceutico/stock.svg" alt="Card image cap">
                                 <div class="card-body">
-                                    <h5 class="card-title">Cargar Stock</h5>
+                                    <h5 class="card-title"> Stock</h5>
                                     <p class="card-text"></p> 
                                     <hr>
-                                    <a href="#" class="btn btn-primary btn-sm">Ir</a>
+                                    <a href="#" class="btn btn-primary btn-sm">cargar</a>
                                 </div>
                         </div>   
                     </div>
                     <div class="col-md-4   col-12">
                         <div class="shadow p-3 mb-5 bg-white rounded">
-                            <img class="card-img-top" width="200" height="200" src="../public/assets/img/imgFarmaceutico/contactar.svg" alt="Card image cap" style="">
+                            <img class="card-img-top" width="150" height="150" src="../public/assets/img/imgFarmaceutico/contactar.svg" alt="Card image cap" style="">
                                 <div class="card-body">
                                     <h5 class="card-title">Contactar al Administardor</h5>
                                     <p class="card-text"></p>
