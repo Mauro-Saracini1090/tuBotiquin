13758d304e913ce129019d45674f27a8a8be7edc
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-11-09 17:33:38 -0300

    Mejoras en el estilo

--- resources/views/farmacia/farmacias.blade.php
+++ resources/views/farmacia/farmacias.blade.php
@@ -6,26 +6,28 @@
         <div class="card-body mb-2">
             <!-- Masthead Subheading-->
             <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Farmacias</h2>
-            <br>  
+            <p class="lead text-center my-3">Listado de las farmacias disponibles</p>
+        
              <form method="POST" action="{{ route('buscarFarmaciaSucursal')}}">
-                 @csrf                     
+                 @csrf           
                     <div class="form-group">
-                        <select id="id_farmacia" class="form-control"  placeholder="Busque su farmacia aqui" name="id_farmacia"> 
-                                    @foreach($arrayFarmacias as $farmacia)
-                                        <option value="{{ $farmacia->id_farmacia }}"> {{ $farmacia->nombre_farmacia }}</option> 
-                                    @endforeach   
-                        </select> 
-                                
-                    </div>
-                    <div class="form-group">
-                        <div class="d-flex d-flex justify-content-center"> 
-                            <button type="submit" class="btn btn-primary mr-1">
-                                {{ __('Buscar') }}
-                            </button>
-                        </div>
-                    </div>
+                        <div class="form-group">
+                            <div class="d-flex d-flex justify-content-center"> 
+                                <select id="id_farmacia" class="form-control"  placeholder="Busque su farmacia aqui" name="id_farmacia"> 
+                                        @foreach($arrayFarmacias as $farmacia)
+                                            <option value="{{ $farmacia->id_farmacia }}"> {{ $farmacia->nombre_farmacia }}</option> 
+                                        @endforeach   
+                                </select> 
+                                <button type="submit" class="btn btn-primary mx-2">
+                                    {{ __('Buscar') }}
+                                </button> 
+                             </div>
+                        </div>         
+                    </div> 
+                </div>           
             </form>
         </div>
+
 
     <div class="container">
         <div class="row">
@@ -34,11 +36,11 @@
                     <div class="col-md-4">
                         <div class="shadow p-2 mb-4 bg-white rounded">          
                             <img class="card-img-top" src="{{ asset($farmaciaPaginate->img_farmacia) }}" alt="Logotipo" width="300" height="250">
-                                <div class="card-body">
+                                <div class="card-body text-center">
                                     <h4 class="card-title"> {{ $farmaciaPaginate->nombre_farmacia }}</h4>
-                                    <p class="card-text">{{ $farmaciaPaginate->descripcion_farmacia }}</p> 
+                                    <p class="font-italic">{{ $farmaciaPaginate->descripcion_farmacia }}</p> 
                                     <hr>
-                                    <a href="{{ route ('farmaciaSucursal', ['farmacia' => $farmaciaPaginate->id_farmacia ]) }}" class="btn btn-primary btn-sm">Ver sucursales</a>
+                                    <a href="{{ route ('farmaciaSucursal', ['farmacia' => $farmaciaPaginate->id_farmacia ]) }}" class="btn btn-link">Ver sucursales</a>
                                 </div>
                         </div>
                     </div> 
