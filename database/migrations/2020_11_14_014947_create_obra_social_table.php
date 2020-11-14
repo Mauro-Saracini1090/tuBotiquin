367d73d746d5e94031ccae5857beac84f014e622
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-11-13 23:04:29 -0300

    Script de Migration

---
+++ database/migrations/2020_11_14_014947_create_obra_social_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateObraSocialTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('obra_social', function (Blueprint $table) {
+            $table->id('id_obra_social');
+            $table->string('Nombre_obra_social', 150);
+            $table->integer('Telefono_obra_Social');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('obra_social');
+    }
+}
