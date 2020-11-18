604cb580755fd550b6c395655bc701d50d161661
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-11-18 20:59:08 -0300

    LEyenda de campos obligatorios

--- resources/views/farmaceutico/cargarFarmacia.blade.php
+++ resources/views/farmaceutico/cargarFarmacia.blade.php
@@ -15,7 +15,7 @@
                         @csrf
 
                         <div class="form-group">
-                            <strong><label  for="nombre_farmacia">{{ __('Nombre de la Farmacia') }}</label></strong>
+                            <strong><label  for="nombre_farmacia">{{ __('Nombre de la Farmacia *') }}</label></strong>
                             <input type="text" name="nombre_farmacia" value="{{ old('nombre_farmacia') }}" @error('nombre_farmacia') is-invalid @enderror required class="form-control">
                              @error('nombre_farmacia')
                                     <span class="invalid-feedback" role="alert">
@@ -24,7 +24,7 @@
                             @enderror
                         </div>
                         <div class="form-group">
-                            <strong><label for="img_farmacia">{{ __('Suba una imagen con su logo') }}</label></strong>
+                            <strong><label for="img_farmacia">{{ __('Suba una imagen con su logo *') }}</label></strong>
                             <input type="file" name="img_farmacia" @error('img_farmacia') accept="image/*" is-invalid @enderror required class="form-control" >
                             <small  class="form-text text-muted">Tamaño máximo 4MB</small>
 
@@ -35,7 +35,7 @@
                             @enderror
                         </div>
                         <div class="form-group">
-                            <strong><label for="descripcion_farmacia">{{ __('Descripción') }}</label></strong>
+                            <strong><label for="descripcion_farmacia">{{ __('Descripción *') }}</label></strong>
                             <textarea class="form-control" name="descripcion_farmacia" type="textarea" placeholder="¡Aqui puede colocar el eslogan de su Farmacia!" @error('descripcion_farmacia') is-invalid @enderror
                                     name="escripcion_farmacia" value="{{ old('descripcion_farmacia') }}"rows="3"></textarea>
 
@@ -46,16 +46,20 @@
                             @enderror
                         </div>
                         <div class="form-group">
-                            <strong><label for="cuit">{{ __('CUIT') }}</label></strong>
+                            <strong><label for="cuit">{{ __('CUIT *') }}</label></strong>
                             <input type="number" name="cuit" value="{{ old('cuit') }}" @error('cuit') is-invalid @enderror required class="form-control" >
-                            <small  class="form-text text-muted">Sin espacios ni guiones</small>
+                            <small  class="form-text text-muted">Sin espacios ni guiones, 8 carácteres mímino</small>
                              @error('cuit')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                             @enderror
                         </div>
-                      
+                        <div class="form-group">
+                                <div class="d-flex d-flex justify-content-left"> 
+                                    <small  class="form-text text-muted">Los campos marcados con (*) son obligatorios</small>
+                                </div>    
+                            </div>
                          <div class="form-group">
                             <div class="d-flex d-flex justify-content-center"> 
                                 <button type="submit" class="btn btn-primary mr-1">
