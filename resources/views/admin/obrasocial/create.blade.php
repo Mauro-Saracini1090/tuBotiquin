b344cc2cc603d1f64ae17f89ddf6b841c8621f4f
Mauro-Saracini1090 <65034599+Mauro-Saracini1090@users.noreply.github.com>
2020-11-18 23:56:06 -0300

    vista para crear obra social - Usuario administrador

---
+++ resources/views/admin/obrasocial/create.blade.php
@@ -0,0 +1,38 @@
+@extends('admin.administrador')
+
+@section('datos')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-2 border-bottom">
+    <h1 class="h2">Crear Obra Social </h1>
+</div>
+@if($errors->any())
+    <div class="alert alert-danger">
+        <p>Por favor corrija los siguientes errores de abajo: </p>
+    </div>
+@endif
+    
+    <form class="form-signin" method="post" action="{{route('obrasocial.store') }}">
+        @csrf
+        <div class="form-group">
+            <label for="Nombre_obra_social">Nombre Obra Social:</label>
+            <input class="form-control" type="text" name="Nombre_obra_social" id="Nombre_obra_social" placeholder="Nombre Obra Social"
+                value="{{ old('Nombre_obra_social')  }}">
+        </div>
+        <div class="form-group">
+            @if($errors->has('Nombre_obra_social'))
+                <p class="text text-danger">{{ $errors->first('Nombre_obra_social') }}</p>
+            @endif
+        </div>
+        <div class="form-group">
+            <label for="Telefono_obra_Social">Telefono:</label>
+            <input class="form-control" type="number" name="Telefono_obra_Social" id="Telefono_obra_Social" placeholder="Telefono" value="{{ old('Telefono_obra_Social') }}">
+
+            @if($errors->has('Telefono_obra_Social'))
+                <p class="text text-danger">{{ $errors->first('Telefono_obra_Social') }}</p>
+            @endif
+
+        </div>
+        <button type="submit" class="btn btn-panel mx-1">Cargar Nueva Obra Social</button>
+        <a href="{{ url()->previous() }}" class="btn btn-secondary mx-1">Volver Atras</a>
+    </form>
+
+@endsection
