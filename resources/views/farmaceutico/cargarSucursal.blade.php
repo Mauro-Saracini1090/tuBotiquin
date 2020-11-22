4cd93bed65d98ba1840ed3c978d2cbf2ef5389e8
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-11-22 13:50:42 -0300

    Se agrego class="form-control" para mostrar los errores de valdiacion del servidor

--- resources/views/farmaceutico/cargarSucursal.blade.php
+++ resources/views/farmaceutico/cargarSucursal.blade.php
@@ -8,7 +8,7 @@
                 <div class="shadow p-3 mb-5 backCard rounded"> 
                     
                         <!-- Masthead Subheading-->
-                        <h3 class="masthead-subheading text-center">Cargar Sucursal</h3>
+                        <h3 class="masthead-subheading text-center">CARGAR SUCURSAL</h3>
                         <p class="lead text-center">Complete los siguientes campos</p>
                         
                         <form method="POST" action="{{ route('sucursal.store') }}">
@@ -28,65 +28,72 @@
                                 </select>
                                 <small  class="form-text text-muted">Si no encuentra su farmacia, contacte al Administrador</small>
                         </div>
+
                        <!-- Descripcion -->
                         <div class="form-group">
                             <strong><label for="descripcion_sucursal">{{ __('Descripción *') }}</label></strong>
-                            <textarea class="form-control" name="descripcion_sucursal" type="textarea" @error('descripcion_sucursal') is-invalid @enderror
-                                    name="descripcion_sucursal" value="{{ old('descripcion_sucursal') }}"rows="3"></textarea>
+                            <textarea  name="descripcion_sucursal" class="form-control is-invalid @error('descripcion_sucursal')  @enderror"
+                                       value="{{ old('descripcion_sucursal') }}" rows="3"></textarea>
+                            <small  class="form-text text-muted">Aca puede colocar los diás y horarios de atención</small>
 
-                                @error('descripcion_sucursal')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            @error('descripcion_sucursal')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+
                         <!-- Cufe -->
                          <div class="form-group">
                             <strong><label  for="cufe_sucursal">{{ __('Cufe sucursal *') }}</label></strong>
-                            <input type="text" name="cufe_sucursal" value="{{ old('cufe_sucursal') }}" @error('cufe_sucursal') is-invalid @enderror required class="form-control">
-                            <small  class="form-text text-muted">Sin espacios ni guiones, 8 dígitos mínimo</small>
+                            <input type="text" name="cufe_sucursal" value="{{ old('cufe_sucursal') }}" class="form-control @error('cufe_sucursal') is-invalid @enderror" required>
+                            <small class="form-text text-muted">Sin espacios ni guiones, 8 dígitos mínimo</small>
 
-                                 @error('cufe_sucursal')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            @error('cufe_sucursal')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+
                          <!-- EMAIL --> 
                         <div class="form-group">
                             <strong><label for="email_sucursal">{{ __('E-mail *') }}</label></strong>
-                            <input type="email" name="email_sucursal" value="{{ old('email_sucursal') }}" @error('email_sucursal') is-invalid @enderror required placeholder="correo@ejemplo.com"class="form-control" >
+                            <input type="email" name="email_sucursal" value="{{ old('email_sucursal') }}" placeholder="correo@ejemplo.com" class="form-control @error('email_sucursal') is-invalid @enderror" required>
                             
-                                 @error('email_sucursal')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            @error('email_sucursal')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+
                          <!-- TELEFONO -->        
                          <div class="form-group">
                             <strong><label for="telefono_sucursal">{{ __('Teléfono *') }}</label></strong>
-                            <input type="text" name="telefono_sucursal" value="{{ old('telefono_sucursal') }}" @error('telefono_sucursal') is-invalid @enderror placeholder="Ejemplo: 29844586958" required class="form-control" >
+                            <input type="text" name="telefono_sucursal" value="{{ old('telefono_sucursal') }}" placeholder="Ejemplo: 29844586958" class="form-control @error('telefono_sucursal') is-invalid @enderror" required>
                             <small  class="form-text text-muted">Sin espacios ni guiones, 8 dígitos mínimo</small>
-                                 @error('telefono_sucursal')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+
+                            @error('telefono_sucursal')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+                        
                         <!-- Direccion -->
                         <div class="form-group">
                             <strong><label for="direccion_sucursal">{{ __('Dirección *') }}</label></strong>
-                        <input type="text" name="direccion_sucursal" value="{{ old('direccion_sucursal') }}" @error('direccion_sucursal') is-invalid @enderror placeholder="Ejemplo: Entre Av. Libertad y San Martin n° 154" required class="form-control" >
+                            <input type="text" name="direccion_sucursal" value="{{ old('direccion_sucursal') }}" placeholder="Ejemplo: Entre Av. Libertad y San Martin n° 154" class="form-control @error('direccion_sucursal') is-invalid @enderror" required>
                             <small  class="form-text text-muted">Calle y número</small>
 
-                                @error('direccion_sucursal')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            @error('direccion_sucursal')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
-                        </div>
+            
                         <div class="form-group">
                             <div class="d-flex d-flex justify-content-left"> 
                                 <small  class="form-text text-muted">Los campos marcados con (*) son obligatorios</small>
