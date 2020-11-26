10c9365b8425403e83aa6445d89d51aa45208377
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-11-25 22:46:46 -0300

    Archivo que tiene conflicto [ ver despues]

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,8 @@ use DatePeriod;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Mail;
+Use App\Mail\MensajeContacto;
 
 class HomeController extends Controller
 {
@@ -26,124 +28,47 @@ class HomeController extends Controller
         $hoy = date('Y-m-d');
         // dd($hoy); 
         $turnos = Turno::where('fecha_turno', '=', $hoy)->first();
-        $sucursalesTurno = [];
-        $arrSucursalesTurnoSiguiente = [];
-        if (isset($turnos)) {
 
+        $sucursalesTurno = $turnos->getSucursales->take(2);
 
-            $sucursalesTurno = $turnos->getSucursales->take(2);
-
-            //Primera Forma Para obtener Dias siguientes
-            //La clase DatePeriod 
-            // (PHP 5 >= 5.3.0, PHP 7)
-            // Representa un período de fechas.
-            // Un período de fechas permite la iteración sobre un conjunto de fechas y horas, repitiéndose a intervalos regulares durante un período dado.
-            // dd($sucursalesTurno);
-            // $periodo = new DatePeriod(new DateTime(), new DateInterval('P1D'), 3);
-            // $fechasSiguientes = array();
-            // foreach ($periodo as $date) {
-            //     array_push($fechasSiguientes, $date->format('Y-m-d'));
-            // }
-            // Segunda Forma, Mas simple pero efectiva
-            $fechasSiguiente1 = date('Y-m-d', strtotime('+1 days'));
-            $fechasSiguiente2 = date('Y-m-d', strtotime('+2 days'));
-            $fechasSiguiente3 = date('Y-m-d', strtotime('+3 days'));
-            $TurnoSiguientes =  Turno::where('fecha_turno', '=', $fechasSiguiente1)->orWhere('fecha_turno', '=', $fechasSiguiente2)->orWhere('fecha_turno', '=', $fechasSiguiente3)->get();
-            $arrSucursalesTurnoSiguiente = array();
-
+        //Primera Forma Para obtener Dias siguientes
+        //La clase DatePeriod 
+        // (PHP 5 >= 5.3.0, PHP 7)
+        // Representa un período de fechas.
+        // Un período de fechas permite la iteración sobre un conjunto de fechas y horas, repitiéndose a intervalos regulares durante un período dado.
+        // dd($sucursalesTurno);
+        // $periodo = new DatePeriod(new DateTime(), new DateInterval('P1D'), 3);
+        // $fechasSiguientes = array();
+        // foreach ($periodo as $date) {
+        //     array_push($fechasSiguientes, $date->format('Y-m-d'));
+        // }
+        // Segunda Forma, Mas simple pero efectiva
+        $fechasSiguiente1 = date('Y-m-d', strtotime('+1 days'));
+        $fechasSiguiente2 = date('Y-m-d', strtotime('+2 days'));
+        $fechasSiguiente3 = date('Y-m-d', strtotime('+3 days'));
+        $TurnoSiguientes=  Turno::where('fecha_turno', '=', $fechasSiguiente1)->orWhere('fecha_turno', '=', $fechasSiguiente2)->orWhere('fecha_turno', '=', $fechasSiguiente3)->get();
+        $arrSucursalesTurnoSiguiente = array();
+       
             foreach ($TurnoSiguientes as $turno) {
                 // array_push($arrSucursalesTurnoSiguiente = $turno->getSucursales->take(3));
                 foreach ($turno->getSucursales->take(2) as $sucursal) {
-                    array_push($arrSucursalesTurnoSiguiente, $sucursal);
+                   array_push($arrSucursalesTurnoSiguiente,$sucursal);
                 }
             }
-        }
+        
+        
         //dd($arrSucursalesTurnoSiguiente);
-        return view('publico.contenidoPrincipal', compact('sucursalesTurno', 'arrSucursalesTurnoSiguiente'));
+        return view('publico.contenidoPrincipal', compact('sucursalesTurno','arrSucursalesTurnoSiguiente'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function mapa()
-    {
-        //
-        return view('admin.mapa.mapa');
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 
     /**
      * Funcion que lista los datos de la farmacia y sucursal (solo una) de turno del home. 
      * Se llama en el boton "ver sucursal" de los cards
      */
     public function verSucursalTurnoHoy(Request $request)
-    {
+    {   
         $sucursal = Sucursal::find($request->id_sucursal);
         $farmacia = Farmacia::find($sucursal->id_farmacia);
         $arrayObraSociales = $farmacia->obrasSociales;
@@ -152,7 +77,7 @@ class HomeController extends Controller
             'sucursal' => $sucursal,
             'farmacia' => $farmacia,
             'arrayObraSociales' => $arrayObraSociales,
-        ]);
+        ]); 
     }
 
 
@@ -162,18 +87,58 @@ class HomeController extends Controller
      */
     public function verSucursalesProximasTurno()
     {
-        $arrayTurnos = Turno::all();
+        //$arrayTurnos = Turno::all();
+        $fechasSiguiente1 = date('Y-m-d', strtotime('+1 days'));
+        $fechasSiguiente2 = date('Y-m-d', strtotime('+2 days'));
+        $fechasSiguiente3 = date('Y-m-d', strtotime('+3 days'));
+        $fechasSiguiente4 = date('Y-m-d', strtotime('+4 days'));
+        $fechasSiguiente5 = date('Y-m-d', strtotime('+5 days'));
+        $fechasSiguiente6 = date('Y-m-d', strtotime('+6 days'));
+        $arrayTurnos =  Turno::where('fecha_turno', '=', $fechasSiguiente1)->orWhere('fecha_turno', '=', $fechasSiguiente2)
+                            ->orWhere('fecha_turno', '=', $fechasSiguiente3)->orWhere('fecha_turno', '=', $fechasSiguiente4)
+                            ->orWhere('fecha_turno', '=', $fechasSiguiente5)->orWhere('fecha_turno', '=', $fechasSiguiente6)->get();
         $arrSucursalDia = array();
         $arrSucursalDiaCompleto = array();
-
-        foreach ($arrayTurnos as  $turno) {
+        
+        foreach ($arrayTurnos as  $turno) {    
             foreach ($turno->getSucursales as $sucursal) {
                 $originalDate = $turno->fecha_turno;
                 $newDate = date("d/m/Y", strtotime($originalDate));
-                $arrSucursalDia = ["sucursal" => $sucursal, "diaTurno" => $newDate];
-                array_push($arrSucursalDiaCompleto, $arrSucursalDia);
-            }
+                 $arrSucursalDia = [ "sucursal" => $sucursal , "diaTurno" => $newDate ];
+                 array_push($arrSucursalDiaCompleto, $arrSucursalDia);   
+            }   
         }
-        return view('publico.verSucursalProximosDias', ['arregloSucursalTurnodia' => $arrSucursalDiaCompleto]);
+        return view('publico.verSucursalProximosDias', [ 'arregloSucursalTurnodia' => $arrSucursalDiaCompleto ]);    
     }
+
+
+    /**
+     * Funcion que invoca el formulario de contacto del menu del home 
+     */
+    public function emailContacto()
+    {
+       return view('publico.emailContacto');
+    }
+
+    /**
+     * Funcion que recibe por post los datos del formulario de contacto
+     */    
+    public function enviarEmailContacto(Request $request)
+    {
+        //Valida los campos del formulario de contacto
+         $request->validate([
+            'nombre' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'asunto' =>'required|max:255',
+            'consulta' => 'required|max:600',
+         ]);
+
+         $variablesContacto = $request;
+         // Si la validacion es se procede a enviar el mail al administrador de Tubotiquín
+         Mail::to('tubotiquin2020@gmail.com')->send(new MensajeContacto($variablesContacto));
+
+        return redirect(route('home'))->with('mensajeEnviado', 'Su mensaje fue enviado con exito, a la brevedad le estaremos respondiendo. Gracias por su consulta');
+
+    }
+
 }
