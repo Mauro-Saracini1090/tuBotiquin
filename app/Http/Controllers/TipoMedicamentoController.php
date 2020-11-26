fe1f0147a5f815da54a19df92aa4ec66026e2529
Mauro-Saracini1090 <65034599+Mauro-Saracini1090@users.noreply.github.com>
2020-11-26 01:15:13 -0300

    Controlador para tipo de Medicamento

---
+++ app/Http/Controllers/TipoMedicamentoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TipoMedicamento;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TipoMedicamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TipoMedicamento  $tipoMedicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TipoMedicamento $tipoMedicamento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TipoMedicamento  $tipoMedicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TipoMedicamento $tipoMedicamento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TipoMedicamento  $tipoMedicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TipoMedicamento $tipoMedicamento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TipoMedicamento  $tipoMedicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TipoMedicamento $tipoMedicamento)
+    {
+        //
+    }
+}
