cb6850646cd264fc176dfa2f706b12bb48794b07
Mauro-Saracini1090 <65034599+Mauro-Saracini1090@users.noreply.github.com>
2020-11-26 01:15:46 -0300

    Model Medicamento

---
+++ app/Models/Medicamento.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Medicamento extends Model
+{
+    use HasFactory;
+
+    protected $table ="medicamentos";
+    protected $primaryKey = 'id_medicamento';
+}
