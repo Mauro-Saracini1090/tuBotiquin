d6931bffdbcd742ae86392cb418c735f2b8b9829
Mauro-Saracini1090 <65034599+Mauro-Saracini1090@users.noreply.github.com>
2020-11-26 01:14:45 -0300

    Controllador para Marca Medicamento

---
+++ app/Http/Controllers/MarcaMedicamentoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MarcaMedicamento;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MarcaMedicamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MarcaMedicamento  $marcaMedicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MarcaMedicamento $marcaMedicamento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MarcaMedicamento  $marcaMedicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MarcaMedicamento $marcaMedicamento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MarcaMedicamento  $marcaMedicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MarcaMedicamento $marcaMedicamento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MarcaMedicamento  $marcaMedicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MarcaMedicamento $marcaMedicamento)
+    {
+        //
+    }
+}
