2bfa72385712b9543a60e152ea024453c291f574
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-11-25 22:01:11 -0300

    Funcion para el envio de mail de los contactos al admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,8 @@ use DatePeriod;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Mail;
+Use App\Mail\MensajeContacto;
 
 class HomeController extends Controller
 {
@@ -59,71 +61,7 @@ class HomeController extends Controller
         return view('publico.contenidoPrincipal', compact('sucursalesTurno','arrSucursalesTurnoSiguiente'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 
     /**
      * Funcion que lista los datos de la farmacia y sucursal (solo una) de turno del home. 
@@ -192,10 +130,14 @@ class HomeController extends Controller
             'nombre' => 'required|max:255',
             'email' => 'required|email|max:255',
             'asunto' =>'required|max:255',
-            'consulta' => 'required||max:255',
+            'consulta' => 'required|max:600',
          ]);
 
-         // ACA FALTA LA PARTE DE ENVIAR EL MAIL
+         $variablesContacto = $request;
+         // Si la validacion es se procede a enviar el mail al administrador de Tubotiquín
+         Mail::to('tubotiquin2020@gmail.com')->send(new MensajeContacto($variablesContacto));
+
+        return redirect(route('home'))->with('mensajeEnviado', 'Su mensaje fue enviado con exito, a la brevedad le estaremos respondiendo. Gracias por su consulta');
 
     }
 
