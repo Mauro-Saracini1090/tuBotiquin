f01d4b2b4b8e376f5b0f36dc611c6e6435162c2b
Mauro-Saracini1090 <65034599+Mauro-Saracini1090@users.noreply.github.com>
2020-12-01 19:54:28 -0300

    formulario crear farmacio para administrador

--- resources/views/admin/farmacias/crearFarmacia.blade.php
+++ resources/views/admin/farmacias/crearFarmacia.blade.php
@@ -12,7 +12,7 @@
                 <form method="POST" action="{{ route('almacenarFarmaciaAdmin') }}"
                     enctype="multipart/form-data">
                     @csrf
-                    <!-- select Farmaceuticos -->
+                    {{-- <!-- select Farmaceuticos -->
                     <div class="form-group">
                         <strong><label for="farmaceutico">{{ __('Seleccione su Farmaceutico') }}</label></strong>
                         <select id="farmaceutico" class="form-control @error('farmaceutico') is-invalid @enderror"
@@ -30,13 +30,12 @@
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
-                    </div>
+                    </div> --}}
                     <div class="form-group">
                         <strong><label
                                 for="nombre_farmacia">{{ __('Nombre de la Farmacia *') }}</label></strong>
                         <input type="text" name="nombre_farmacia"
-                            value="{{ old('nombre_farmacia') }}"
-                            @error('nombre_farmacia') is-invalid @enderror required class="form-control">
+                            value="{{ old('nombre_farmacia') }}" required class="form-control @error('nombre_farmacia') is-invalid @enderror">
                         @error('nombre_farmacia')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -47,8 +46,7 @@
                         <strong><label
                                 for="img_farmacia">{{ __('Suba una imagen con su logo *') }}</label></strong>
                         <input type="file" name="img_farmacia"
-                            value="{{ old('img_farmacia') }}"
-                            @error('img_farmacia') accept="image/*" is-invalid @enderror class="form-control">
+                            value="{{ old('img_farmacia') }}" class="form-control  @error('img_farmacia')is-invalid @enderror" accept="image/*">
                         <small class="form-text text-muted">Tamaño máximo 4MB</small>
 
                         @error('img_farmacia')
@@ -60,10 +58,8 @@
                     <div class="form-group">
                         <strong><label
                                 for="descripcion_farmacia">{{ __('Descripción *') }}</label></strong>
-                        <textarea class="form-control" name="descripcion_farmacia" type="textarea"
-                            placeholder="¡Aqui puede colocar el eslogan de su Farmacia!" @error('descripcion_farmacia')
-                            is-invalid @enderror
-                            name="descripcion_farmacia">{{ old('descripcion_farmacia') }}</textarea>
+                        <textarea class="form-control @error('descripcion_farmacia') is-invalid @enderror" name="descripcion_farmacia" type="textarea"
+                            placeholder="¡Aqui puede colocar el eslogan de su Farmacia!"  name="descripcion_farmacia">{{ old('descripcion_farmacia') }}</textarea>
 
                         @error('descripcion_farmacia')
                             <span class="invalid-feedback" role="alert">
@@ -74,8 +70,7 @@
                     <div class="form-group">
                         <strong><label for="cuit">{{ __('CUIT *') }}</label></strong>
                         <input type="number" name="cuit"
-                            value="{{ old('cuit') }}" @error('cuit')
-                            is-invalid @enderror required class="form-control">
+                            value="{{ old('cuit') }}" required class="form-control @error('cuit') is-invalid @enderror">
                         <small class="form-text text-muted">Sin espacios ni guiones - 8 caracteres mínimo</small>
                         @error('cuit')
                             <span class="invalid-feedback" role="alert">
