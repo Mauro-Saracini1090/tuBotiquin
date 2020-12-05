c5c01440a1527837532f65462ba0bb8c07f48b75
Mauro-Saracini1090 <65034599+Mauro-Saracini1090@users.noreply.github.com>
2020-12-05 01:59:57 -0300

    model farmacia funcion scope para buscador

--- app/Models/Farmacia.php
+++ app/Models/Farmacia.php
@@ -10,7 +10,7 @@ class Farmacia extends Model
     use HasFactory;
     protected $table = "farmacia";
     protected $primaryKey = 'id_farmacia';
-    
+
     protected $fillable = [
         'id_usuario',
         'nombre_farmacia',
@@ -24,22 +24,28 @@ class Farmacia extends Model
     // Para obtener el dueno de la farmacia
     public function usuarioFarmaceutico()
     {
-        return $this->belongsTo(Usuario::class,'id_usuario');
-    
+        return $this->belongsTo(Usuario::class, 'id_usuario');
     }
 
     //Para obtener todas las sucursales
     public function getSucursales()
     {
-        return $this->hasMany(Sucursal::class,'id_farmacia','id_farmacia');
+        return $this->hasMany(Sucursal::class, 'id_farmacia', 'id_farmacia');
     }
 
 
 
     public function obrasSociales()
     {
-        return $this->belongsToMany(ObraSocial::class, 'obra_social_farmacia','farmacia_id','obra_social_id');
+        return $this->belongsToMany(ObraSocial::class, 'obra_social_farmacia', 'farmacia_id', 'obra_social_id');
     }
-    
 
+    //Scope nombre farmacia
+    public function scopeNombreFarmacia($query, $nombreFarmacia)
+    {
+        if ($nombreFarmacia) {
+            # code...
+            return $query->where('nombre_farmacia', 'LIKE', "%$nombreFarmacia%");
+        }
+    }
 }
