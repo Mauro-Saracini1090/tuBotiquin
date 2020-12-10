7b95cf09412520695b89b7a10dd36bc798a1b119
Mauro-Saracini1090 <65034599+Mauro-Saracini1090@users.noreply.github.com>
2020-12-10 01:11:57 -0300

    vista mail reserva rechazada

---
+++ resources/views/emails/Farmaceutico/reservaRechazada.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tu Botiquin - Solicitud de Reserva Rechazada</h1>
+    <p>Su reserva fue rechazada - cominiquese con la sucursal: , si cree que esto es un error.</p>
+</body>
+</html>
