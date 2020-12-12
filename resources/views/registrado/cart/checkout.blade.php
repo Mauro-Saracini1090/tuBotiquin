2d1203386c09855d8f8dad8582a5872922eb0f7c
Mauro-Saracini1090 <65034599+Mauro-Saracini1090@users.noreply.github.com>
2020-12-12 16:36:14 -0300

    vista carrito de reservas

--- resources/views/registrado/cart/checkout.blade.php
+++ resources/views/registrado/cart/checkout.blade.php
@@ -2,68 +2,19 @@
 @section('titulo', 'Farmacias')
 
 @section('contenido')
-<!-- Section of  alert mail contac send -->
-@if(session()->has('borrado'))
-    <div class="container">
-        <div class="row">
-            <div class="col-12 mx-auto">
-                <div class="text-left alert alert-success alert-dismissible fade show" role="alert">
-                    <p class="font-weight-bold">{{ session()->get('borrado') }}</p>
-                    <strong>
-                        <p class="text-right">Equipo TuBotiquín</p>
-                    </strong>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            </div>
-        </div>
+<div class="container">
+    <div class="card-body">
+        <!-- Masthead Subheading-->
+        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Reservas</h2>
+        <p class="lead text-center my-3">Listado de Reservas Realizadas</p>
     </div>
-@endif
-<!-- Section of  alert mail contac send -->
-@if(session()->has('reservaconfirmada'))
-    <div class="container">
-        <div class="row">
-            <div class="col-12 mx-auto">
-                <div class="text-left alert alert-success alert-dismissible fade show" role="alert">
-                    <p class="font-weight-bold">{{ session()->get('reservaconfirmada') }}</p>
-                    <strong>
-                        <p class="text-right">Equipo TuBotiquín</p>
-                    </strong>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-@endif
-<!-- Section of  alert mail contac send -->
-@if(session()->has('overflow'))
-    <div class="container">
-        <div class="row">
-            <div class="col-12 mx-auto">
-                <div class="text-left alert alert-warning alert-dismissible fade show" role="alert">
-                    <p class="font-weight-bold">{{ session()->get('overflow') }}</p>
-                    <strong>
-                        <p class="text-right">Equipo TuBotiquín</p>
-                    </strong>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-@endif
-@can('esRegistrado')
-    <!-- Section of  Cancelar Reserva -->
-    @if(session()->has('cancelar'))
+    <!-- Section of  alert mail contac send -->
+    @if(session()->has('borrado'))
         <div class="container">
             <div class="row">
                 <div class="col-12 mx-auto">
-                    <div class="text-left alert alert-warning alert-dismissible fade show" role="alert">
-                        <p class="font-weight-bold">{{ session()->get('cancelar') }}</p>
+                    <div class="text-left alert alert-success alert-dismissible fade show" role="alert">
+                        <p class="font-weight-bold">{{ session()->get('borrado') }}</p>
                         <strong>
                             <p class="text-right">Equipo TuBotiquín</p>
                         </strong>
@@ -75,64 +26,123 @@
             </div>
         </div>
     @endif
-@endcan
-<div class="container">
+    <!-- Section of  alert mail contac send -->
+    @if(session()->has('reservaconfirmada'))
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mx-auto">
+                    <div class="text-left alert alert-success alert-dismissible fade show" role="alert">
+                        <p class="font-weight-bold">{{ session()->get('reservaconfirmada') }}</p>
+                        <strong>
+                            <p class="text-right">Equipo TuBotiquín</p>
+                        </strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+    <!-- Section of  alert mail contac send -->
+    @if(session()->has('overflow'))
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mx-auto">
+                    <div class="text-left alert alert-warning alert-dismissible fade show" role="alert">
+                        <p class="font-weight-bold">{{ session()->get('overflow') }}</p>
+                        <strong>
+                            <p class="text-right">Equipo TuBotiquín</p>
+                        </strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+    @can('esRegistrado')
+        <!-- Section of  Cancelar Reserva -->
+        @if(session()->has('cancelar'))
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 mx-auto">
+                        <div class="text-left alert alert-warning alert-dismissible fade show" role="alert">
+                            <p class="font-weight-bold">{{ session()->get('cancelar') }}</p>
+                            <strong>
+                                <p class="text-right">Equipo TuBotiquín</p>
+                            </strong>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+    @endcan
     <div class="row">
         <div class="col-sm-12 bg-light">
             @if(count(Cart::getContent()))
-                    <table class="table table-striped">
-                        <thead>
-                            <th>FARMACIA</th>
-                            <th>NOMBRE</th>
-                            <th>CANTIDAD</th>
-                            <th>STOCK TOTAL DISPONIBLE</th>
-                        </thead>
-                        <tbody>
-                            @foreach(Cart::getContent() as $item)
-                                
-                                <tr>
-                                    <td>{{ $farmacia->nombre_farmacia }}</td>
-                                    <td>{{ $item->name }}</td>
-                                    <td>{{ $item->quantity }}</td>
-                                    <td>{{ $item->total }}</td>
-                                    <td>
-                                        <form action="{{ route('cart.removeitem') }}" method="POST">
-                                            @csrf
-                                            <input type="hidden" name="id" value="{{ $item->id }}">
-                                            <input type="hidden" name="total" value="{{ $item->quantity }}">
-                                            <input type="hidden" name="farmacia" value="{{ $farmacia }}">
-                                            <button type="submit" class="btn btn-link btn-sm text-danger">X</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                <table class="table table-striped">
+                    <thead>
+                        <th>FARMACIA</th>
+                        <th>NOMBRE</th>
+                        <th>CANTIDAD</th>
+                        <th>STOCK TOTAL DISPONIBLE</th>
+                    </thead>
+                    <tbody>
+                        @foreach(Cart::getContent() as $item)
 
-                    <form method="POST" action="{{ route('cart.confirmar-reserva') }}">
+                            <tr>
+                                <td>{{ $farmacia->nombre_farmacia }}</td>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->quantity }}</td>
+                                <td>{{ $item->total }}</td>
+                                <td>
+                                    <form action="{{ route('cart.removeitem') }}" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="id" value="{{ $item->id }}">
+                                        <input type="hidden" name="total" value="{{ $item->quantity }}">
+                                        <input type="hidden" name="farmacia" value="{{ $farmacia }}">
+                                        <button type="submit" class="btn btn-link btn-sm text-danger">X</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                <form method="POST" action="{{ route('cart.confirmar-reserva') }}">
                     @csrf
                     @foreach(Cart::getContent() as $item)
-                   
+
                         <input type="hidden" name="id" value="{{ $item->id }}">
                         <input name="medicamentos[]" type="hidden" name="id" value="{{ $item->id }}">
-                        <input name="cantidad[{{ $item->id }}]" type="hidden" name="id" value="{{ $item->quantity }}">
+                        <input name="cantidad[{{ $item->id }}]" type="hidden" name="id"
+                            value="{{ $item->quantity }}">
 
                     @endforeach
 
                     <!-- select Farmacias -->
                     <div class="form-group">
                         <strong>
-                            <label for="id_sucursal">{{ __('Seleccione una Sucursal para el Retiro *') }}
+                            <label
+                                for="id_sucursal">{{ __('Seleccione una Sucursal para el Retiro *') }}
                             </label>
                         </strong>
                         <select id="id_sucursal" class="form-control @error('id_sucursal') is-invalid @enderror"
                             name="id_sucursal" value="{{ old('id_sucursal') }}" required>
                             <option></option>
-                            
+
                             @foreach($farmacia->getSucursales as $item)
+                            @if($item->habilitado == 1)
                                 <option value="{{ $item->id_sucursal }}">
-                                    {{ $item->getFarmacia->nombre_farmacia }} - Direccion: {{ $item->direccion_sucursal }}
+                                    {{ $item->getFarmacia->nombre_farmacia }} - Direccion:
+                                    {{ $item->direccion_sucursal }}
                                 </option>
+                                @endif
                             @endforeach
                         </select>
                         <small class="form-text text-muted">
