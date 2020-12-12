249b0b5f8a60d2f84c70b71e2dcd55850ea9958c
Mauro-Saracini1090 <65034599+Mauro-Saracini1090@users.noreply.github.com>
2020-12-12 16:36:39 -0300

    historial reservaregistrado

--- resources/views/registrado/historialReservaRegistrado.blade.php
+++ resources/views/registrado/historialReservaRegistrado.blade.php
@@ -7,16 +7,20 @@
         <div class="col-sm-12 bg-light">
             <table class="table table-striped">
                 <thead>
+                    <tr>
+                        <th></th>
                     <th>Numero Reserva</th>
                     <th>Sucursal</th>
                     <th>Estado</th>
                     <th>Informacion</th>
                     <th>Fecha Solicitud</th>
                     <th>Fecha Vencimiento</th>
+                    </tr>
                 </thead>
                 <tbody>
                     @foreach($reservas as $reserva)
                         <tr>
+                            <td></td>
                             <td>{{ $reserva->numero_reserva }}</td>
                             <td>{{ $reserva->getSucursal->getFarmacia->nombre_farmacia }} -
                                 {{ $reserva->getSucursal->direccion_sucursal }}</td>
@@ -37,7 +41,11 @@
 
                 </tbody>
             </table>
+            <div class="d-flex d-flex justify-content-center mt-4"> 
+                {{ $reservas->links() }}
+            </div>
         </div>
     </div>
 </div>
 @endsection
+
