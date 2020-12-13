a3a9b8d37437dc0599599d729988a532a8a9a3a6
Mauro-Saracini1090 <65034599+Mauro-Saracini1090@users.noreply.github.com>
2020-12-13 02:39:02 -0300

    Vista editar marca medicamento para administrador

--- resources/views/admin/marcaMedicamento/editarMarcaMedicamento.blade.php
+++ resources/views/admin/marcaMedicamento/editarMarcaMedicamento.blade.php
@@ -4,8 +4,6 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-2 border-bottom">
     <h1 class="h2">Editar Marca de Medicamento</h1>
 </div>
-
-    
     <form class="form-signin" method="post" action="{{route('marcaMedicamentos.update',[$marcaMedicamento]) }}">
         @csrf
         @method('PATCH')
@@ -20,6 +18,6 @@
             @endif
         </div>
         <button type="submit" class="btn btn-panel mx-1">Editar Marca Medicamento</button>
-        <a href="{{ url()->previous() }}" class="btn btn-secondary mx-1">Volver Atras</a>
+        <a href="{{ route('marcaMedicamentos.index') }}" class="btn btn-secondary mx-1">Volver Atras</a>
     </form>
 @endsection
